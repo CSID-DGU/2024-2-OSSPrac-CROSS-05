287a8929c2cbf009532b0e4ec79bbd87cf953296
Lee SeungHo <126765002+sholee-pt@users.noreply.github.com>
2024-11-06 23:42:40 +0900

    Change folder/file name

--- Subject3_1/ex4.py
+++ Subject3_1/ex4.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('input.html')
 
 @app.route('/result', methods=['POST'])
 def result():
