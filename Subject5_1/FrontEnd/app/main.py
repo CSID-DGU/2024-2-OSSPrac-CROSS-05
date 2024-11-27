d43d47989944d49ee49d05be8683b246e61348f4
sholee-pt <sholeept@dgu.ac.kr>
2024-11-27 13:46:56 +0900

    Divide FE, BE from Subject3_2

--- Subject5_1/FrontEnd/app/main.py
+++ Subject5_1/FrontEnd/app/main.py
@@ -3,49 +3,36 @@ import requests
 
 app = Flask(__name__)
 
-# '/' 경로로 index.html 렌더링
+# 입력 페이지 렌더링
 @app.route('/')
-def index():
-    return render_template('index.html')
+def main():
+    return render_template('input_info.html')
 
-# '/input' 경로로 input.html 렌더링
-@app.route('/input')
-def input_page():
-    return render_template('input.html')
-
-# '/result' 경로로 POST 요청 처리
+# 결과 페이지 렌더링
 @app.route('/result', methods=['POST'])
-def process_data():
-    # 사용자가 입력한 데이터를 수집
-    team_data = request.form.to_dict(flat=False)
-    print("Data sent to backend:", team_data)  # 디버깅용 출력
+def result():
+    # 사용자 입력 데이터를 수집
+    form_data = {
+        "Name": request.form.get('Name'),
+        "StudentNumber": request.form.get('StudentNumber'),
+        "Gender": request.form.get('Gender'),
+        "Major": request.form.get('Major'),
+        "PL_list": request.form.getlist('PL_list')
+    }
 
-    # Backend로 데이터 전송
-    backend_url = "http://backapp:5001/process"
-    try:
-        response = requests.post(backend_url, json=team_data)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        return f"Error communicating with backend: {e}", 500
+    # 백엔드로 데이터 전달 및 결과 수신
+    response = requests.post('http://backapp:5001/process', data=form_data)
+    result_data = response.text.split('|')  # 플레인 텍스트 파싱
 
-    # Backend에서 반환된 데이터 확인
-    if response.status_code == 200:
-        processed_data = response.json()
-        print("Data received from backend:", processed_data)  # 디버깅용 출력
-        return render_template('result.html', team_name=processed_data['team_name'], teamMembers=processed_data['team_members'])
-    else:
-        return f"Error from backend: {response.status_code} {response.text}", 500
-
-
-# '/contact' 경로로 연락처 정보 렌더링
-@app.route('/contact')
-def contact():
-    # URL에서 전달받은 데이터
-    name = request.args.get('name')
-    email = request.args.get('email')
-    phone = request.args.get('phone')
-    return render_template('contact.html', name=name, email=email, phone=phone)
+    # 결과 페이지 렌더링
+    return render_template(
+        'result.html',
+        Name=result_data[0],
+        StudentNumber=result_data[1],
+        Gender=result_data[2],
+        Major=result_data[3],
+        Languages=result_data[4]
+    )
 
 if __name__ == '__main__':
-    # Flask 앱 실행
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)  # FrontEnd는 5000번 포트에서 실행
